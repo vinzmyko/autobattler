960b6fedb0fcd6e4c5a8171e76a7ef2c2761006e
Vinz <vinzmykodelrosario@gmail.com>
2025-02-03 22:08:03 +0000

    refactor(arena): centralise unit service init in arena

--- scenes/arena/Arena.cs
+++ scenes/arena/Arena.cs
@@ -5,4 +5,15 @@ public partial class Arena : Node2D
     public static readonly Vector2 CELL_SIZE = new Vector2(32, 32);
     public static readonly Vector2 HALF_CELL_SIZE = new Vector2(16, 16);
     public static readonly Vector2 QUARTER_CELL_SIZE = new Vector2(8, 8);
+
+    public UnitMover UnitMoverService;
+    public UnitSpawner UnitSpawnerService;
+
+    public override void _Ready()
+    {
+        UnitMoverService = GetNode<UnitMover>("UnitMover");
+        UnitSpawnerService = GetNode<UnitSpawner>("UnitSpawner");
+
+        UnitSpawnerService.UnitSpawned += UnitMoverService.SetupUnit;
+    }
 }
